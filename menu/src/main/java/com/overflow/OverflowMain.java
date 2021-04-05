9809e731f558b93342cea21b7b6d6db808c8883c
batman084 <awesomegautam1@gmai.com>
2021-04-04 22:23:21 -0700

    Test build #2

--- menu/src/main/java/com/overflow/OverflowMain.java
+++ menu/src/main/java/com/overflow/OverflowMain.java
@@ -2,13 +2,27 @@ package com.overflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.sql.Driver;
 
 @SpringBootApplication
-public class OverflowMain {
+public class OverflowMain extends SpringBootServletInitializer {
     @RequestMapping(value = "/index")
     public String index() {
         return "index";
@@ -17,4 +31,11 @@ public class OverflowMain {
     public static void main(String[] args) throws IOException, GeneralSecurityException {
         SpringApplication.run(OverflowController.class, args);
     }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
 }
+
