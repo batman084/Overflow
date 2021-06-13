1ac2a9cec88142c11ffa87af6de1f867d448696a
Gautam Gupta <50473039+batman084@users.noreply.github.com>
2021-06-13 13:05:07 -0700

    Adding ToDo to Main (Jacob) (#25)
    ToDo Commit by Jacob

---
+++ todo/src/main/java/com/overflow/todo/database/IDbConnection.java
@@ -0,0 +1,17 @@
+package com.overflow.todo.database;
+
+import com.overflow.todo.data.TodoItem;
+
+import java.util.ArrayList;
+
+public interface IDbConnection {
+    TodoItem update(TodoItem item);
+
+    TodoItem remove(TodoItem item);
+
+    TodoItem find(int id);
+
+    ArrayList<TodoItem> findAllForUser(int userId);
+
+    void clearAll();
+}
