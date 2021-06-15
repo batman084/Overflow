312f18d1946e9e123f2ef3febbb46f49b5e63add
jacob <jar04.san@outlook.com>
2021-06-14 18:03:00 -0700

    To Do controller

--- todo/src/main/java/com/overflow/todo/tests/ServiceTest.java
+++ todo/src/main/java/com/overflow/todo/tests/ServiceTest.java
@@ -14,8 +14,7 @@ public class ServiceTest {
 
     private static void runTests() {
         // create in working directory folder with a specific name
-        // DbConnection db = DbConnection.createConnection("", "serviceTest.db");
-        DbConnection db = DbConnection.createConnection();
+        DbConnection db = DbConnection.createConnection("", "serviceTest.db");
 
         TodoService service = new TodoService(db);
 
@@ -44,5 +43,18 @@ public class ServiceTest {
         System.out.println("Looking for items after a specific date...");
         items = service.find(TodoItem.createDate(2021, 1, 2, 0, 0));
         System.out.println("expected 1 items and found: " + items.size());
+    }
+
+    public static void generateSampleItems() {
+        DbConnection db = DbConnection.createConnection();
+
+        TodoService service = new TodoService(db);
+
+        System.out.println("inserting 4 items for default user and 1 item for a different user...");
+        service.update(new TodoItem(0, 0, "(1) Math", "Math assignment is due", TodoItem.createDate(2021, 6, 15, 13, 30)));
+        service.update(new TodoItem(0, 0, "(2) Biology", "Biology assignment is due", TodoItem.createDate(2021, 6, 15, 14, 30)));
+        service.update(new TodoItem(0, 0, "(3) Chemistry", "Chemistry assignment is due", TodoItem.createDate(2021, 6, 15, 10, 30)));
+        service.update(new TodoItem(0, 0, "(4) Physics", "Physics assignment is due", TodoItem.createDate(2021, 6, 16, 9, 0)));
+        service.update(new TodoItem(0, 1, "Another Assignment", "An assignment for another user", TodoItem.createDate(2021, 1, 3, 9, 0)));
     }
 }
